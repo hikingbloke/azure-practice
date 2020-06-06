8d8e5d36a57375bddeb68d465a40300014c2483d
Bipin Sawant <bipin.sawant@gmail.com>
2020-06-06 00:16:24 -0700

    first commit
    my experiments with Azure

---
+++ README.md
@@ -0,0 +1,9 @@
+# My experiments with Azure
+
+FeatureKey API is a microservice for getting and validating WatchGuard's device feature keys.
+
+## Development Tools
+
+- [Visual Studio 2019](https://www.visualstudio.com/downloads/)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [.NET Core 3.1](https://www.microsoft.com/net/download)
---
+++ implement-atuhentrication -and-secure-data/get-secret-get-encryptionkey-from-keyvault/Program.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using System;
+using System.Text;
+
+namespace KeyVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSecretFromKeyVault();
+            GetEncyptionKeysFromKeyVault();
+        }
+
+        private static void GetEncyptionKeysFromKeyVault()
+        {
+            string keyVaultUrl = "https://securedatakeyvault01.vault.azure.net/";
+            var client = new KeyClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+            // Getting the Encryption key from the key vault
+            KeyVaultKey key = client.GetKey("newkey");
+
+            var cryptoClient = new CryptographyClient(keyId: key.Id, credential: new DefaultAzureCredential());
+            byte[] plaintext = Encoding.UTF8.GetBytes("This is sensitive data");
+
+            // Encrypting data
+            EncryptResult encryptResult = cryptoClient.Encrypt(EncryptionAlgorithm.RsaOaep256, plaintext);
+
+            //Decrypting data
+            DecryptResult decryptResult = cryptoClient.Decrypt(EncryptionAlgorithm.RsaOaep256, encryptResult.Ciphertext);
+            Console.WriteLine("Plain text");
+
+            string txt = Encoding.UTF8.GetString(decryptResult.Plaintext);
+            Console.WriteLine(txt);
+            Console.ReadLine();
+        }
+
+        private static void GetSecretFromKeyVault()
+        {
+            string keyVaultUrl = "https://securedatakeyvault01.vault.azure.net/";
+            var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+            KeyVaultSecret secret = client.GetSecret("dbpassword");
+            Console.WriteLine(secret.Value);
+            Console.ReadKey();
+        }
+    }
+}
